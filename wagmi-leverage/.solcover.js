57a662ee0916fcf5bb9a6fca7cc6b91d277749fe
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2023-10-11 20:32:16 +0200

    Delete scope directories

--- wagmi-leverage/.solcover.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    skipFiles: ['mock', 'vendor0.8'],
-    configureYulOptimizer: true,
-    mocha: {
-        grep: "@skip-on-coverage", // Find everything with this tag
-        invert: true               // Run the grep's inverse set.
-    }
-};
